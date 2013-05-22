4e3914a92ece4753e155601990ba65b56ceb401a
Eric Krause <eric.krause@gmail.com>
2013-05-22 10:54:29 -0400

    whitespace cleanup

--- lib/omniauth/strategies/app_direct.rb
+++ lib/omniauth/strategies/app_direct.rb
@@ -14,7 +14,7 @@ module OmniAuth
         :city => 'http://axschema.org/contact/city/home',
         :state => 'http://axschema.org/contact/state/home',
         :website => 'http://axschema.org/contact/web/default',
-        :image => 'http://axschema.org/media/image/aspect11', 
+        :image => 'http://axschema.org/media/image/aspect11',
         :roles => 'https://www.appdirect.com/schema/user/roles'
       }
 
@@ -32,7 +32,7 @@ module OmniAuth
         i = env['rack.session']['openid_url'] if env && env['rack.session'] && env['rack.session']['openid_url']
         i
       end
-      
+
       def get_identifier
         env['rack.session']['openid_url'] || 'https://www.appdirect.com/openid/id'
       end
@@ -51,19 +51,19 @@ module OmniAuth
         openid_response.message.get_arg('http://openid.net/srv/ax/1.0', attribute)
       end
 
-      # Parsing the roles out of the return schema. 
-      # Roles might be in the value.ext4 field if there is only one, 
-      # or possibly in the value.ext4.N fields if there are many. 
+      # Parsing the roles out of the return schema.
+      # Roles might be in the value.ext4 field if there is only one,
+      # or possibly in the value.ext4.N fields if there are many.
       # Assume the worst: check both places  and reject any nils from the result.
       def roles
         [get_appdirect_attr('value.ext4')].tap do |roles|     # Roles array starts with one value: value.ext4 (possibly nil)
-          get_appdirect_attr('count.ext4').to_i.times do |i|  # Then we use the roles count to cycle through each role variable 
+          get_appdirect_attr('count.ext4').to_i.times do |i|  # Then we use the roles count to cycle through each role variable
             roles << get_appdirect_attr("value.ext4.#{i+1}")  # And shift it into the array
           end
-        end.reject {|v| !v } # remove nils 
+        end.reject {|v| !v } # remove nils
       end
 
-      extra do          
+      extra do
         { :roles => roles }
       end
 
@@ -74,4 +74,3 @@ end
 OmniAuth.config.add_camelization 'appdirect', 'AppDirect'
 OmniAuth.config.add_camelization 'app_direct', 'AppDirect'
 OmniAuth.config.add_camelization 'app-direct', 'AppDirect'
-
